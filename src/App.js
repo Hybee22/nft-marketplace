e14fad07aca1b8fb5b6650065a40fa4d971b532a
Ibrahim Adekunle <adefemi101@gmail.com>
2022-04-07 14:09:55 +0100

    initial commit for showcase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Features, SectionWrapper, Download } from "./components";
 
-function App() {
+import assets from "./assets";
+import styles from "./styles/Global";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper
+        title="Your own store of Hybee NFTs. Start Selling and Growing"
+        description="Buy, store, collect NFTs, exchange and earn crypto. Join 35+ million using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a sleek UI of ProNef NFT Marketplace. Smooth and beautiful colors of a fluent UI Design"
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users' devices."
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains the list and details pages for the NFTs"
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by <span className="bold">Ibrahim</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
